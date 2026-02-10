f8245cd8c3d3f3aa1ac6f04a5832f6cbaef23694
Felix J. Blanco C. <Felixblancocabrera@gmail.com>
2026-02-10 00:30:15 -0400

    Feature: Fixed BEM CSS variables for better understanding, improved css of tasklists and the items.

--- src/components/TaskHolder.tsx
+++ src/components/TaskHolder.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type { Todo } from "./model";
 import "./TaskHolder.css";
+import Header from "./Header";
 type TodosProps = {
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
@@ -9,13 +10,17 @@ type TodosProps = {
 // set todos in case we add delete here etc.
 const TaskHolder: React.FC<TodosProps> = ({ todos, setTodos }) => {
   return (
-      <ol className="card">
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            <p className="card__list">{todo.todo}</p>
-          </li>
-        ))}
-      </ol>
+    <div>
+
+<Header className="heading">Undone Tasks</Header>
+    <ol className="task__list">
+      {todos.map((todo) => (
+        <li key={todo.id} className="task__item">
+          {todo.todo}
+        </li>
+      ))}
+    </ol>
+</div>
   );
 };
 
